b81188a541eec8808649b4cd87592840ea1821b7
mxwrlld <maksim.mixwell@gmail.com>
2020-10-01 17:41:17 +0500

    init: solving several common problems of working with arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if( toString.call( array ) != '[object Array]' || array.length == 0) {
+        return 0;
+    }
+    let min = array[0];
+    for (let i = 0; i < array.length; ++i) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if ( toString.call( array ) != '[object Array]' || array.length == 0) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 0; i < array.length; ++i) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return  max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if( toString.call( array ) != '[object Array]' || array.length == 0) {
+        return 0;
+    }
+    let sum = 0;
+    for(let i = 0; i < array.length; ++i) {
+        sum += array[i];
+    }
+    let avg = sum/array.length;
+    return avg;
+};
